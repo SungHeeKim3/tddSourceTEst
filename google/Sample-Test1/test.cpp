17dbf2df02eaa485caa55bb106cca1f58b10145c
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-16 16:59:44 +0900

    update class when they have more than one param

--- google/Sample-Test1/test.cpp
+++ google/Sample-Test1/test.cpp
@@ -22,3 +22,8 @@ TEST_F(PrimeFactorFixture, of3) {
 	expected = { 3 };
 	EXPECT_EQ(expected, prime_factor.of(3));
 }
+
+TEST_F(PrimeFactorFixture, of4) {
+	expected = { 2,2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
