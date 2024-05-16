dc13273a81054310b624dbe94b9b0f1213dbfee5
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-16 16:53:27 +0900

    make FixtureClass

--- Project30/prime-factor.cpp
+++ Project30/prime-factor.cpp
@@ -1,11 +1,15 @@
 #include <vector>
 using namespace std;
-class FrimeFactor
+class PrimeFactor
 {
 public:
 	vector<int> of(int i)
 	{
 		vector<int> result = {};
+		if (i == 2)
+		{
+			result.push_back(2);
+		}
 		return result;
 	}
 };
--- google/Sample-Test1/test.cpp
+++ google/Sample-Test1/test.cpp
@@ -1,8 +1,19 @@
 #include "pch.h"
 #include "../../Project30/prime-factor.cpp"
 
-TEST(TestCaseName, TestName) {
-	FrimeFactor pr;
-	vector<int> expected{};
-	EXPECT_EQ(expected, pr.of(1));
+class PrimeFactorFixture : public testing::Test
+{
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFactorFixture, of1) {
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFactorFixture, of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
